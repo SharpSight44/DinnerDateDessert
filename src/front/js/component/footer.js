7785436b4eee59695df64295c5a0f45d9cdc2f5a
Karinohana <Karinohana95@gmail.com>
2022-06-08 20:33:44 +0000

    Footer social media links

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
-    <div>>>>>>>>>>>>Footer Goes Here>>>>>>>>>>></div>
+    <div>
+      <a href="https://facebook.com" rel="noreferrer">
+        Facebook
+      </a>
+      <div></div>
+      <a href="https://instagram.com" rel="noreferrer">
+        Instagram
+      </a>
+    </div>
   </footer>
 );
--- src/front/js/component/memberNavbar.js
+++ src/front/js/component/memberNavbar.js
@@ -2,28 +2,28 @@ import React from "react";
 
 export const MemberNavbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-success bg-success">
       <div className="container-fluid">
         <Link to="/account">
           <span className="navbar-brand mb-0 h1">Account</span>
         </Link>
         <div className="ml-auto">
           <Link to="/tailor">
-            <button className="btn btn-primary">Tailor</button>
+            <button className="btn btn-dark">Tailor</button>
           </Link>
         </div>
         <div className="ml-auto">
           <Link to="/desires">
-            <button className="btn btn-primary">Desires</button>
+            <button className="btn btn-dark">Desires</button>
           </Link>
         </div>
         <div className="ml-auto">
           <Link to="/memories">
-            <button className="btn btn-primary">Memories</button>
+            <button className="btn btn-dark">Memories</button>
           </Link>
           <div className="ml-auto">
             <Link to="/upcomingoutings">
-              <button className="btn btn-primary">Upcoming Outings</button>
+              <button className="btn btn-dark">Upcoming Outings</button>
             </Link>
           </div>
         </div>
