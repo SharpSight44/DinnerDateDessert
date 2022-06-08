086340e194859234615e24fa40e8badb042f0941
elash1312 <lasherin1312@gmail.com>
2022-06-08 01:00:16 +0000

    member navbar path fixed

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { HeroBanner } from "../component/herobanner";
-<<<<<<< HEAD
-
-
-=======
 import { HomePageCard } from "../component/homepagecard";
 import { Navbar } from "../component/navbar";
->>>>>>> 065b19e (herobanner, cards started)
 
 export const Home = () => {
   return (
     <>
-<<<<<<< HEAD
-
-=======
       <Navbar />
->>>>>>> 065b19e (herobanner, cards started)
       <div className="container-fluid">
         <div className="row">
           <div className="col"></div>
