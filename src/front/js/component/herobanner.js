3a09b74611a7dc2e30d35af1a22190280f19fdc7
elash1312 <lasherin1312@gmail.com>
2022-06-09 00:10:34 +0000

    card text modified

--- src/front/js/component/herobanner.js
+++ src/front/js/component/herobanner.js
@@ -11,12 +11,6 @@ export const HeroBanner = () => {
               <img src={banner} alt="Banner" />
             </div>
           </div>
-          <div className="row">
-            <div className="col">
-              <h1>Allow us to curate your next memory to last a lifetime</h1>
-              <h3>Dinner Date Dessert is your premiere date curating service. Based on a quick and easy questionnaire, we will create the perfect date experience that you have in mind, based on occasion and preferences set by you!</h3>
-            </div>
-          </div>
         </div>
       </div>
     </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,19 +7,21 @@ export const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col"></div>
-        </div>
-        <HeroBanner />
-        <div className="row">
-          <div className="col">
-            <HomePageCard />
+      <div className="homebody">
+        <div className="container">
+          <div className="row">
+            <div className="col"></div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <h1>Call to Action/Signup</h1>
+          <HeroBanner />
+          <div className="row">
+            <div className="col">
+              <HomePageCard />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h1>Call to Action/Signup</h1>
+            </div>
           </div>
         </div>
       </div>
