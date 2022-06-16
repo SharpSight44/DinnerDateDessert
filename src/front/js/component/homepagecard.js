20137bc363d43bb1da60b11bc15eae4f26c6ace2
elash1312 <lasherin1312@gmail.com>
2022-06-16 20:12:13 +0000

    margin fixed on homepage

--- src/front/js/component/homepagecard.js
+++ src/front/js/component/homepagecard.js
@@ -4,7 +4,7 @@ export const HomePageCard = () => {
     return (
         <>
             <div className="row row-cols-1 row-cols-md-3 g-4 ">
-                <div className="col">
+                <div className="col justify-content-center">
                     <div className="card border-0">
                         <img src="https://media.istockphoto.com/photos/romantic-evening-picture-id649672132?k=20&m=649672132&s=612x612&w=0&h=8-BY5q_zcMdkvgYi0kY2d1rDgO6MwrGdJ08pEpCsPTQ=" className="card-img-top" alt="..." />
                         <div className="card-body rounded">
@@ -13,7 +13,7 @@ export const HomePageCard = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col justify-content-center">
                     <div className="card border-0">
                         <img src="https://t3.ftcdn.net/jpg/03/16/15/72/360_F_316157216_5naUgMIsN0HJwzcUFDVIyJQHciI5aNkV.jpg" className="card-img-top" alt="..." />
                         <div className="card-body rounded">
@@ -22,7 +22,7 @@ export const HomePageCard = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col justify-content-center">
                     <div className="card border-0">
                         <img src="https://cdn.create.vista.com/api/media/medium/234029416/stock-photo-boyfriend-feeding-woman-sweet-dessert?token=" className="card-img-top" alt="..." />
                         <div className="card-body rounded">
