abdb73a50b30e6599b39e23b51a719dace705b9a
Karinohana <Karinohana95@gmail.com>
2022-06-16 22:56:03 +0000

    survey

--- src/front/js/component/survey.js
+++ src/front/js/component/survey.js
@@ -7,11 +7,53 @@ import { Survey } from "survey-react-ui";
 StylesManager.applyTheme("modern");
 
 const surveyJson = {
-  elements: [
-    { name: "FirstName", title: "Enter your first name:", type: "text" },
-    { name: "LastName", title: "Enter your last name:", type: "text" },
+  title: "test",
+  description: "test",
+  logoPosition: "right",
+  pages: [
+    {
+      name: "page1",
+      elements: [
+        {
+          type: "matrix",
+          name: "test question",
+          title: "Q1",
+          hideNumber: true,
+          columns: [
+            {
+              value: "Column 1",
+              text: "Avoid",
+            },
+            {
+              value: "Column 2",
+              text: "Don't Care",
+            },
+            {
+              value: "Column 3",
+              text: "Neutral",
+            },
+            {
+              value: "Column 4",
+              text: "Like",
+            },
+            {
+              value: "Column 5",
+              text: "Love!",
+            },
+          ],
+          rows: ["Row 1"],
+        },
+      ],
+    },
+    {
+      name: "page2",
+    },
+    {
+      name: "page3",
+    },
   ],
 };
+
 function Surveyone() {
   const survey = new Model(surveyJson);
   return <Survey model={survey} />;
