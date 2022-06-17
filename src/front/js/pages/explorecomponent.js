b50be96b413ad5a0d9edb1834af6a9f4b6a851f9
elash1312 <lasherin1312@gmail.com>
2022-06-17 18:30:27 +0000

    explorepage

--- src/front/js/pages/explorecomponent.js
+++ src/front/js/pages/explorecomponent.js
@@ -1,157 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../../styles/explore.css";
+import { getApiRestaurants, getApiDesserts, getApiEvents } from '../component/apiExplore';
+
 
 export const ExploreComponent = () => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [desserts, setDesserts] = useState([]);
+
+    useEffect(() => {
+        const fn = async () => {
+            const apiRest = await getApiRestaurants();
+            return setRestaurants(apiRest);
+        };
+        fn();
+    }, []);
+    useEffect(() => {
+        const fn = async () => {
+            const apiParty = await getApiEvents();
+            return setEvents(apiParty);
+        };
+        fn();
+    }, []);
+    useEffect(() => {
+        const fn = async () => {
+            const apiDess = await getApiDesserts();
+            return setDesserts(apiDess);
+        };
+        fn();
+    }, []);
+
     return (
         <>
-            <div className="container">
-                <h1 className="titles">Dinner</h1>
-                <div className="row scroll">
-                    <div className="card text-white text-end fw-bolder">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/ga1cBI0dJA8RXys4jWRxAA/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Casa Latina 1986</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/_xG4snKnfofCLXi5zgRYVw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">  Bourbon Steak</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/0JPciU78x67fW3Tal88qFw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Old's Havana</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/D7aodMheWlaS3Ye6iED2-Q/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Minty Z</h2>                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/kfBQFTygXAuqFqwgy74jkw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Black Market Miami</h2>                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/wZPJuwTYSvf9TkEBC3eNtQ/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Grails Miami</h2>                        </div>
-                    </div>
 
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/7AyL_38ORPXnORStQ_Klcw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Craft Coral Gables</h2>                        </div>
-                    </div>
+            <div className='container'>
+                <h1 className="titles">A Bite to Eat</h1>
+                <div className='row scroll'>{restaurants.map((item, index) =>
 
-                </div>
-                <h1 className="titles">Date</h1>
-                <div className="row scroll">
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/-bzUMsP25FY_MF_B2tbitw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Pinecrest Garden</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/h6yVRN4BsICZ9FM_pDfCcw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Axe Habits</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/ws7Gs-qjObljckD_8wrqcQ/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Virginia Key Outdoor Center</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/cDZGa2Uf9-y0xHGHdyi42Q/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Vizcaya Museum & Gardens</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/CEcDC2ARiJ7ibeB3szCh3g/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Ultraboat Rentals</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/4ygZ8qoZnVLYeSN67WZNjA/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">The Kampong</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/Xc5b16quanbczYV82oJ6uQ/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Luna Bohemia</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white text-end">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/UhcYAn16ho4u2aLxlUFD7w/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Clear View Kayaks</h2>
-                        </div>
-                    </div>
-                </div>
-                <h1 className="titles">Dessert</h1>
-                <div className="row scroll">
-                    <div className="card text-white">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/adLN8B8sqvbPkxx0gYV0Zw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">SugarDaddy Miami</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/o37S2dzeWtBKeDNUV_h0FA/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Bachour</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/ytVk_rBK5U9EnWWpnc682Q/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Mr Kream</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/WxGiuzPUTe93XfYSsri3Iw/348s.jpg" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Miami Baked</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="..." className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Card title</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="..." className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Card title</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="..." className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Card title</h2>
-                        </div>
-                    </div>
-                    <div className="card text-white">
-                        <img src="..." className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <h2 className="innertitle">Card title</h2>
-                        </div>
-                    </div>
-                    <div className='col'></div>
-                </div>
-            </div >
+                    <div className="card text-white text-end fw-bolder" key={index} style={{ width: "300px" }} > <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} /><div className="card-img-overlay"><h2 className="innertitle">{item?.name} </h2> </div></div>)}</div>
+
+                <h1 className="titles">A Date</h1>
+                <div className='row scroll'> {events.map((item, index) =>
+
+                    <div className="card text-white text-end fw-bolder" key={index} style={{ width: "300px" }} > <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} /><div className="card-img-overlay"><h2 className="innertitle">{item?.name} </h2> </div></div>)} </div>
+
+                <h1 className="titles">Something Sweet</h1>
+                <div className='row scroll'>{desserts.map((item, index) =>
+
+                    <div className="card text-white text-end fw-bolder" key={index} style={{ width: "300px" }} > <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} /><div className="card-img-overlay"><h2 className="innertitle">{item?.name} </h2> </div></div>)} </div>
+
+            </div>
         </>
     );
 }
