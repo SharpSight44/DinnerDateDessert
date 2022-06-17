3347fee578d857476f12f46c7f65e069125b1b5d
elash1312 <lasherin1312@gmail.com>
2022-06-17 20:28:37 +0000

    styling to explore component

--- src/front/js/pages/explorecomponent.js
+++ src/front/js/pages/explorecomponent.js
@@ -34,25 +34,26 @@ export const ExploreComponent = () => {
         <>
 
             <div className='container'>
+                <h1 className="pagetitle text-center">Explore </h1>
                 <h1 className="titles">A Bite to Eat</h1>
                 <div className='row scroll'>{restaurants.map((item, index) =>
 
-                    <div className="card text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
-                        <a className="innertitle" href={item.url}>
+                    <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
+                        <a href={item.url}>
                             <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                             <div className="card-img-overlay d-flex align-items-end justify-content-end">
-                                <h2>{item?.name} </h2>
+                                <h2 className='innertitle'>{item?.name} </h2>
                             </div>
                         </a>
                     </div>
                 )}
                 </div>
 
-                <h1 className="titles">A Date</h1>
+                <h1 className="titles">Activities</h1>
                 <div className='row scroll'> {events.map((item, index) =>
 
-                    <div className="card text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
-                        <a className="innertitle" href={item.url}>
+                    <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
+                        <a href={item.url}>
                             <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                             <div className="card-img-overlay d-flex align-items-end justify-content-end">
                                 <h2 className="innertitle">{item?.name} </h2>
@@ -64,8 +65,8 @@ export const ExploreComponent = () => {
                 <h1 className="titles">Something Sweet</h1>
                 <div className='row scroll'>{desserts.map((item, index) =>
 
-                    <div className="card text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
-                        <a className="innertitle" href={item.url}>
+                    <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
+                        <a href={item.url}>
                             <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                             <div className="card-img-overlay d-flex align-items-end justify-content-end">
                                 <h2 className="innertitle">{item?.name} </h2>
