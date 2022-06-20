921d9afce12970892836f832f3b6591c31555b5a
Karinohana <Karinohana95@gmail.com>
2022-06-20 00:54:26 +0000

    Memories Layout

--- src/front/js/component/memberhome/memories.js
+++ src/front/js/component/memberhome/memories.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { MemberNavbar } from "../memberNavbar";
+import TitlebarImageList from "../memberhome/memoriescard";
 
 export const Memories = () => {
-    return (
-        <>
-        <MemberNavbar/>
-        <h1 style={{fontSize:"33px", color:"white"}}>ALL Items from Outings WENT On.. will go here --Think of how to make it memorable? upload pictures? Journal experience?? </h1>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col"></div>
-                </div>
-                <div className="row">
-                    <div className="col"></div>
-                </div>
-                <div className="row">
-                    <div className="col"></div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <MemberNavbar />
+      <TitlebarImageList />
+      <h1 style={{ fontSize: "33px", color: "white" }}>
+        ALL Items from Outings WENT On.. will go here --Think of how to make it
+        memorable? upload pictures? Journal experience??{" "}
+      </h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+        </div>
+      </div>
+    </>
+  );
 };
