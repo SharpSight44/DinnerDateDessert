3366b93ee3ac33ddcc4239e8525b11f6867f984b
elash1312 <lasherin1312@gmail.com>
2022-06-21 21:56:44 +0000

    scroll button implemented

--- src/front/js/pages/explorecomponent.js
+++ src/front/js/pages/explorecomponent.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import "../../styles/explore.css";
 import { getApiRestaurants, getApiDesserts, getApiEvents } from '../component/apiExplore';
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
 
 
 export const ExploreComponent = () => {
     const [restaurants, setRestaurants] = useState([]);
     const [events, setEvents] = useState([]);
     const [desserts, setDesserts] = useState([]);
-    console.log(events);
+
+    const slideLeft = () => {
+        const slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    const slideRight = () => {
+        const slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
+
     useEffect(() => {
         const fn = async () => {
             const apiRest = await getApiRestaurants();
@@ -36,20 +48,22 @@ export const ExploreComponent = () => {
             <div className='container'>
                 <h1 className="pagetitle text-center">Explore </h1>
                 <h1 className="categories">A Bite to Eat</h1>
-                <div className='row scroll'>{restaurants.map((item, index) =>
-
+                <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeft} /></div>
+                <div id="slider" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth'>{restaurants.map((item, index) =>
                     <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
                         <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                         <div className="card-img-overlay d-flex align-items-end justify-content-end">
-                            {/* <button className="btn favsicon border-0 d-flex justify-content-start rounded-circle">♡</button> */}
+                            <button className="btn favsicon border-0 d-flex justify-content-start rounded-circle"><AiOutlineHeart /></button>
                             <a href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
                         </div>
                     </div>
                 )}
                 </div>
+                <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRight} /></div>
 
                 <h1 className="categories">Activities</h1>
-                <div className='row scroll'> {events.map((item, index) =>
+                <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeft} /></div>
+                <div id="slider" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth'> {events.map((item, index) =>
 
                     <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
                         <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
@@ -59,6 +73,7 @@ export const ExploreComponent = () => {
                         </div>
                     </div>)}
                 </div>
+                <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRight} /></div>
 
                 <h1 className="categories">Something Sweet</h1>
                 <div className='row scroll'>{desserts.map((item, index) =>
