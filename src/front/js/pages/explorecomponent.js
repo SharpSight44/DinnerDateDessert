04abece16bc96165d35f5fcca8da0b56b60ee157
elash1312 <lasherin1312@gmail.com>
2022-06-22 20:35:50 +0000

    fixed explore component orientation

--- src/front/js/pages/explorecomponent.js
+++ src/front/js/pages/explorecomponent.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import "../../styles/explore.css";
 import { getApiRestaurants, getApiDesserts, getApiEvents } from '../component/apiExplore';
-import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
-import { AiOutlineHeart } from "react-icons/ai";
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill, BsFillHeartFill } from "react-icons/bs";
+import { BiHeartCircle } from "react-icons/bi";
+import { RiHeartsLine } from "react-icons/ri";
 
 
 export const ExploreComponent = () => {
     const [restaurants, setRestaurants] = useState([]);
     const [events, setEvents] = useState([]);
     const [desserts, setDesserts] = useState([]);
+
 
     const slideLeft = () => {
         const slider = document.getElementById('slider')
@@ -17,6 +19,26 @@ export const ExploreComponent = () => {
 
     const slideRight = () => {
         const slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
+
+    const slideLeftTwo = () => {
+        const slider = document.getElementById('slider2')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    const slideRightTwo = () => {
+        const slider = document.getElementById('slider2')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
+
+    const slideLeftThree = () => {
+        const slider = document.getElementById('slider3')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    const slideRightThree = () => {
+        const slider = document.getElementById('slider3')
         slider.scrollLeft = slider.scrollLeft + 500
     }
 
@@ -47,46 +69,61 @@ export const ExploreComponent = () => {
 
             <div className='container'>
                 <h1 className="pagetitle text-center">Explore </h1>
-                <h1 className="categories">A Bite to Eat</h1>
-                <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeft} /></div>
-                <div id="slider" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth'>{restaurants.map((item, index) =>
-                    <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
-                        <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
-                        <div className="card-img-overlay d-flex align-items-end justify-content-end">
-                            <button className="btn favsicon border-0 d-flex justify-content-start rounded-circle"><AiOutlineHeart /></button>
-                            <a href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                <div className='mb-3'>
+                    <h1 className="categories">A Bite to Eat</h1>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeft} /></div>
+                        <div id="slider" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth m-0'>{restaurants.map((item, index) =>
+                            <div className="card explorecard text-white text-end fw-bolder mx-2 bg-transparent border-0" key={index} style={{ width: "300px" }} >
+                                <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
+                                <div className="card-img-overlay d-flex align-items-end justify-content-between">
+                                    <button className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
+                                    <a className='locationname' href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                                </div>
+                            </div>
+                        )}
+                        </div>
+                        <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRight} />
                         </div>
                     </div>
-                )}
                 </div>
-                <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRight} /></div>
 
-                <h1 className="categories">Activities</h1>
-                <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeft} /></div>
-                <div id="slider" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth'> {events.map((item, index) =>
 
-                    <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
-                        <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
-                        <div className="card-img-overlay d-flex align-items-end justify-content-end">
-                            {/* <button className="btn favsicon border-0 d-flex justify-content-start rounded-circle">♡</button> */}
-                            <a href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                <div className='mb-3'>
+                    <h1 className="categories">Activities</h1>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeftTwo} /></div>
+                        <div id="slider2" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth m-0'> {events.map((item, index) =>
+
+                            <div className="card explorecard text-white text-end fw-bolder mx-2 bg-transparent border-0" key={index} style={{ width: "300px" }} >
+                                <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
+                                <div className="card-img-overlay d-flex align-items-end justify-content-end">
+                                    <button className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
+                                    <a className='locationname' href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                                </div>
+                            </div>)}
                         </div>
-                    </div>)}
+                        <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRightTwo} /></div>
+                    </div>
                 </div>
-                <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRight} /></div>
 
-                <h1 className="categories">Something Sweet</h1>
-                <div className='row scroll'>{desserts.map((item, index) =>
+                <div className='mb-3'>
+                    <h1 className="categories">Something Sweet</h1>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div className="leftarrow rounded-circle justify-content-start"><BsFillArrowLeftCircleFill onClick={slideLeftThree} /></div>
+                        <div id="slider3" className='row scroll overflow-x-scroll whitespace-nowrap scroll-smooth m-0'>{desserts.map((item, index) =>
 
-                    <div className="card explorecard text-white text-end fw-bolder" key={index} style={{ width: "300px" }} >
-                        <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
-                        <div className="card-img-overlay d-flex align-items-end justify-content-end">
-                            {/* <button className="btn favsicon border-0 d-flex justify-content-start rounded-circle">♡</button> */}
-                            <a href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                            <div className="card explorecard text-white text-end fw-bolder mx-2 bg-transparent border-0" key={index} style={{ width: "300px" }} >
+                                <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
+                                <div className="card-img-overlay d-flex align-items-end justify-content-end">
+                                    <button className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
+                                    <a className='locationname' href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                                </div>
+                            </div>)}
                         </div>
-                    </div>)}
+                        <div className="rightarrow rounded-circle justify-content-end"><BsFillArrowRightCircleFill onClick={slideRightThree} /></div>
+                    </div>
                 </div>
-
             </div>
         </>
     );
