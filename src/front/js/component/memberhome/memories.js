8193bccef2d3f7247d8fe71c01a2244da11a03f9
Karinohana <Karinohana95@gmail.com>
2022-06-23 23:35:25 +0000

    Memories survey css

--- src/front/js/component/memberhome/memories.js
+++ src/front/js/component/memberhome/memories.js
@@ -2,11 +2,14 @@ import React from "react";
 import { MemberNavbar } from "../memberNavbar";
 import TitlebarImageList from "../memberhome/memoriescard";
 import Surveytwo from "../memoriessurvey";
+
 export const Memories = () => {
   return (
     <>
       <MemberNavbar />
-      <Surveytwo />
+      <div className="memsurvey">
+        <Surveytwo />
+      </div>
       <h1 className="memoriesheader">See your memories here!</h1>
       <TitlebarImageList />
       <div className="container-fluid">
