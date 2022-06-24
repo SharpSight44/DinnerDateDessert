f770f3003be6e20a1567fb67c417ace0891cc659
Karinohana <Karinohana95@gmail.com>
2022-06-24 21:02:36 +0000

    footer logo

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import glassesicon from "../../img/glassesicon.jpg";
 
 export const Footer = () => (
   <footer className="footer mt-20 py-3 text-center">
     <div>
+      <a href="" rel="noreferrer">
+        <img className="footerlogo" src={glassesicon} alt="logo" />
+      </a>
       <a href="https://facebook.com" rel="noreferrer">
         <FacebookIcon />
       </a>
