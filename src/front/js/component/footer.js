fc81bb97122ab8fc7640fb297863007b9ff45621
Karinohana <Karinohana95@gmail.com>
2022-06-25 15:40:30 +0000

    footer icons

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import glassesicon from "../../img/glassesicon.jpg";
+import { FaGooglePlay } from "react-icons/fa";
+import { GrAppleAppStore } from "react-icons/gr";
 
 export const Footer = () => (
   <footer className="footer mt-20 py-3 text-center">
@@ -9,10 +11,28 @@ export const Footer = () => (
       <a href="" rel="noreferrer">
         <img className="footerlogo" src={glassesicon} alt="logo" />
       </a>
-      <a href="https://facebook.com" rel="noreferrer">
+      <a
+        href="https://play.google.com/store/games"
+        rel="noreferrer"
+        className="googlelogo"
+      >
+        <FaGooglePlay />
+      </a>
+      <a
+        href="https://www.apple.com/app-store/"
+        rel="noreferrer"
+        className="applelogo"
+      >
+        <GrAppleAppStore />
+      </a>
+      <a href="https://facebook.com" rel="noreferrer" className="facebookicon">
         <FacebookIcon />
       </a>
-      <a href="https://instagram.com" rel="noreferrer">
+      <a
+        href="https://instagram.com"
+        rel="noreferrer"
+        className="instagramicon"
+      >
         <InstagramIcon />
       </a>
     </div>
