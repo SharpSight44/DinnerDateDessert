2dbf709a59da2de5df388ec41823ebe3cdffdd82
Karinohana <Karinohana95@gmail.com>
2022-06-29 22:14:08 +0000

    checkbox

--- src/front/js/component/recipecard.js
+++ src/front/js/component/recipecard.js
@@ -39,11 +39,7 @@ export default function RecipeReviewCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
+        avatar={<Avatar aria-label="recipe">R</Avatar>}
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
@@ -64,7 +60,7 @@ export default function RecipeReviewCard(props) {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="completed outing">
+        <IconButton color="success" aria-label="completed outing">
           <CheckCircleIcon />
         </IconButton>
         <ExpandMore
