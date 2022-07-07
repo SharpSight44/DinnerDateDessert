3244f67797c2f9000d8946695c9465436e737db0
elash1312 <lasherin1312@gmail.com>
2022-07-07 02:05:23 +0000

    modified explore component links to open in new windo

--- src/front/js/pages/explorecomponent.js
+++ src/front/js/pages/explorecomponent.js
@@ -7,7 +7,7 @@ import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill, BsFillHeartFill 
 import { BiHeartCircle } from "react-icons/bi";
 import { RiHeartsLine } from "react-icons/ri";
 import { DataBaseChange, GlobalZipCode } from '../layout';
-import { soMa, upperHaight, missionDistrict, theMarina, financial,southBeach, wynwood,doral,brickell, upperEastSide, soho, brooklyn } from '../component/memberhome/Engine Room/activitesData';
+import { soMa, upperHaight, missionDistrict, theMarina, financial, southBeach, wynwood, doral, brickell, upperEastSide, soho, brooklyn } from '../component/memberhome/Engine Room/activitesData';
 
 
 export const ExploreComponent = () => {
@@ -15,8 +15,8 @@ export const ExploreComponent = () => {
     const [restaurants, setRestaurants] = useState([]);
     const [events, setEvents] = useState([]);
     const [desserts, setDesserts] = useState([]);
-    const {zipCode, setZipCode}= useContext(GlobalZipCode);
-    const {dataUpdate, setDataUpdate} = useContext(DataBaseChange);
+    const { zipCode, setZipCode } = useContext(GlobalZipCode);
+    const { dataUpdate, setDataUpdate } = useContext(DataBaseChange);
     console.log(store.desires);
     // console.log(events);
     // console.log(restaurants);
@@ -67,72 +67,72 @@ export const ExploreComponent = () => {
         };
         fn();
     }, [zipCode]);
-  
-    useEffect(()=>{
 
-        if(zipCode== 94107){
+    useEffect(() => {
+
+        if (zipCode == 94107) {
             return setEvents(soMa);
-          } if (zipCode == 94110)
-          { return setEvents(missionDistrict);
-        
-          } if(zipCode== 94133){
+        } if (zipCode == 94110) {
+            return setEvents(missionDistrict);
+
+        } if (zipCode == 94133) {
             return setEvents(theMarina);
-          } if (zipCode == 94117)
-          { return setEvents(upperHaight);
-        
-          }if(zipCode== 100004){
-            return setEvents(financial);
-          } if (zipCode == 10012)
-          { return setEvents(soho);
-        
-          }if(zipCode== 10065){
-            return setEvents(upperEastSide);
-          } if (zipCode == 11205)
-          { return setEvents(brooklyn);
-        
-          }if(zipCode== 33129){
-            return setEvents(brickell);
-          } if (zipCode == 33139)
-          { return setEvents(southBeach);
-        
-          }if(zipCode== 33127){
-            return setEvents(wynwood);
-          } if (zipCode == 33166)
-          { return setEvents(doral);
-        
-          }
-          else {
+        } if (zipCode == 94117) {
             return setEvents(upperHaight);
-          }
+
+        } if (zipCode == 100004) {
+            return setEvents(financial);
+        } if (zipCode == 10012) {
+            return setEvents(soho);
+
+        } if (zipCode == 10065) {
+            return setEvents(upperEastSide);
+        } if (zipCode == 11205) {
+            return setEvents(brooklyn);
+
+        } if (zipCode == 33129) {
+            return setEvents(brickell);
+        } if (zipCode == 33139) {
+            return setEvents(southBeach);
+
+        } if (zipCode == 33127) {
+            return setEvents(wynwood);
+        } if (zipCode == 33166) {
+            return setEvents(doral);
+
+        }
+        else {
+            return setEvents(upperHaight);
+        }
 
 
     }, [zipCode]
 
-    ); 
+    );
 
-    const saveDinner = (item)=>{
+    const saveDinner = (item) => {
 
 
-        const itemData = {dinner:item?.name, dinImg:item?.image_url, dinLoc:item?.location?.display_address, dinRating:item?.rating};
-        const render = dataUpdate + 1 ;
+        const itemData = { dinner: item?.name, dinImg: item?.image_url, dinLoc: item?.location?.display_address, dinRating: item?.rating };
+        const render = dataUpdate + 1;
 
         return postDinner(itemData), setDataUpdate(render);
 
     };
-    const saveDate = (item)=>{
+    const saveDate = (item) => {
 
 
-        const itemData = {dinner:item?.name, dinImg:item?.image_url, dinLoc:item?.location?.display_address, dinRating:item?.rating};
-        const render = dataUpdate + 1 ;
+        const itemData = { dinner: item?.name, dinImg: item?.image_url, dinLoc: item?.location?.display_address, dinRating: item?.rating };
+        const render = dataUpdate + 1;
 
         return postDate(itemData), setDataUpdate(render);
 
     };
-    const saveDessert = (item)=>{
+    const saveDessert = (item) => {
 
 
-        const itemData = {dessert:item?.name, desImg:item?.image_url, desLoc:item?.location?.display_address, desRating:item?.rating};
-        const render = dataUpdate + 1 ;
+        const itemData = { dessert: item?.name, desImg: item?.image_url, desLoc: item?.location?.display_address, desRating: item?.rating };
+        const render = dataUpdate + 1;
 
         return postDessert(itemData), setDataUpdate(render);
 
@@ -155,8 +155,8 @@ export const ExploreComponent = () => {
                             <div className="card explorecard text-white text-end fw-bolder mx-2 bg-transparent border-0" key={index} style={{ width: "300px" }} >
                                 <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                                 <div className="card-img-overlay d-flex align-items-end justify-content-between">
-                                    <button onClick={() => saveDinner(item) } className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
-                                    <a className='locationname' href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                                    <button onClick={() => saveDinner(item)} className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
+                                    <a className='locationname' href={item.url} target="_blank"><h2 className='locationname'>{item?.name} </h2></a>
                                 </div>
                             </div>
                         )}
@@ -177,7 +177,7 @@ export const ExploreComponent = () => {
                                 <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                                 <div className="card-img-overlay d-flex align-items-end justify-content-end">
                                     <button onClick={() => saveDate(item)} className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
-                                    <a className='locationname' href={item.event_site_url}><h2 className='locationname'>{item?.name} </h2></a>
+                                    <a className='locationname' href={item.event_site_url} target="_blank"><h2 className='locationname'>{item?.name} </h2></a>
                                 </div>
                             </div>)}
                         </div>
@@ -195,7 +195,7 @@ export const ExploreComponent = () => {
                                 <img src={item?.image_url} className="card-img" alt="..." style={{ width: "300px" }} />
                                 <div className="card-img-overlay d-flex align-items-end justify-content-end">
                                     <button onClick={() => saveDessert(item)} className="favsicon bg-transparent border-0 d-flex justify-content-start rounded-circle border border-dark">♡</button>
-                                    <a className='locationname' href={item.url}><h2 className='locationname'>{item?.name} </h2></a>
+                                    <a className='locationname' href={item.url} target="_blank"><h2 className='locationname'>{item?.name} </h2></a>
                                 </div>
                             </div>)}
                         </div>
